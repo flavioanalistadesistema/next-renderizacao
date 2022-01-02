14b029bad854961f1dcf7f52931b34de4fdd4d36
Flavio Santosm <flavioanalistadesistema@gmail.com>
2022-01-02 13:50:19 -0300

    feat: Componente estático #3 - revalidar conteudo

---
+++ pages/estatico3.jsx
@@ -0,0 +1,17 @@
+export function getStaticProps() {
+    return {
+        props: {
+            numero: Math.random()
+        },
+        revalidate: 5
+    }
+}
+
+export default function Estatico2(props) {
+    return (
+        <div>
+            <h1>Estico #3</h1>
+            <span>{props.numero}</span>
+        </div>
+    )
+}
