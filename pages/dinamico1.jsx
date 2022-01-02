2e3ff8fd1cadcbade1f66a1e0826c34c9ff73b07
Flavio Santosm <flavioanalistadesistema@gmail.com>
2022-01-02 15:12:10 -0300

    feat: Componente dinamico #1 - criando componente dinamico

---
+++ pages/dinamico1.jsx
@@ -0,0 +1,21 @@
+
+export function getServerSideProps() {
+    console.log('[SERVER] gerando compoente dinamico' )
+
+    return ({
+        props: {
+            numero: Math.random()
+        }
+    })
+
+}
+
+export default function Dinamico1(props) {
+
+    return (
+        <div>
+            <h1>Dinamico #1</h1>
+            <h2>{props.numero}</h2>
+        </div>
+    )
+}
